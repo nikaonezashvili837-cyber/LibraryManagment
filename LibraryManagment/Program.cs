4efb83f896cc6da90acea0e6ede431dfabb34ccf
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-06-05 21:29:24 +0400

    Creatig greetings and user menus

--- LibraryManagment/Program.cs
+++ LibraryManagment/Program.cs
@@ -1,6 +1,8 @@
-﻿namespace LibraryManagment
+﻿
+
+namespace LibraryManagment
 {
-    class Program
+    partial class Program
     {
         public static void Main()
         {
@@ -13,31 +15,47 @@
             ======================
             ");
             int menuOption = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter username:");
+            string? userName = Console.ReadLine();
+            InitilizeMenu(userName, menuOption);
+        }
+        public static void InitilizeMenu(string? userName, int menuOption)
+        {
+            User user = new User(userName);
+            Console.WriteLine($@"
+            
+            ==========================
+            {user.WelcomeMessage()}
+            ==========================");
+            string librarianMenu = @"
+            ===== LIBRARIAN MENU =====
+            1. Add a new item
+            2. Remove an item
+            3. View all items
+            4. Register a new member
+            5. View all members and their borrowed items
+            6. Exit
+            ==========================";
+            string memberMenu = @"
+            ===== MEMBER MENU =====
+            1. Search for an item
+            2. View available items
+            3. Borrow an item
+            4. Return an item
+            5. View my borrowed items
+            6. Exit
+            ==========================";
             if (menuOption == 1)
             {
-                Console.WriteLine(@"
-                ===== LIBRARIAN MENU =====
-                1. Add a new item
-                2. Remove an item
-                3. View all items
-                4. Register a new member
-                5. View all members and their borrowed items
-                6. Exit
-                ==========================
-                ");
+                Console.WriteLine(librarianMenu);
+                int? librerianMenuOption = Convert.ToInt32(Console.ReadLine());
             }
-            else if (menuOption == 2)
+            else
             {
-                Console.WriteLine(@"
-                ===== MEMBER MENU =====
-                1. Search for an item
-                2. View available items
-                3. Borrow an item
-                4. Return an item
-                5. View my borrowed items
-                6. Exit
-                =======================");
+                Console.WriteLine(memberMenu);
+                int? memberMenuOption = Convert.ToInt32(Console.ReadLine());
             }
         }
     }
+
 }
